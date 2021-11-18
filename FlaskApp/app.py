f52d1064f044c73fab11c68c7708a938c205774f
ShivaliGupta-git <94637587+ShivaliGupta-git@users.noreply.github.com>
2021-11-18 11:47:21 -0600

    Add files via upload

---
+++ FlaskApp/app.py
@@ -0,0 +1,8 @@
+
+from website import create_app
+
+app = create_app()
+       
+
+if __name__ == "__main__":        # on running python app.py
+    app.run()                     # run the flask app
